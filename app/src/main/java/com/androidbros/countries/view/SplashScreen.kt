651cc4fe723f0dc79f021fc4e1cc1152f58ec488
adematici <adematicis41@gmail.com>
2021-09-11 15:37:48 +0300

    SplashScreen.kt completed

--- app/src/main/java/com/androidbros/countries/view/SplashScreen.kt
+++ app/src/main/java/com/androidbros/countries/view/SplashScreen.kt
@@ -1,9 +1,43 @@
 package com.androidbros.countries.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.androidbros.countries.R
+import com.androidbros.countries.ui.theme.SplashBackgroundColor
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        navController.navigate("country_list_screen") {
+            popUpTo("splash_screen") { inclusive = true }
+        }
+    }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = SplashBackgroundColor
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.earth),
+                contentDescription = "earth",
+                modifier = Modifier.size(150.dp)
+            )
+        }
+    }
 }
