10f1379a05b71f3cfa31872298ddd9678e358a68
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2021-09-11 17:20:44 +0300

    Topbar, Country Card and scaffold created

--- app/src/main/java/com/androidbros/countries/MainActivity.kt
+++ app/src/main/java/com/androidbros/countries/MainActivity.kt
@@ -3,6 +3,7 @@ package com.androidbros.countries
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -11,14 +12,17 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import coil.annotation.ExperimentalCoilApi
 import com.androidbros.countries.ui.theme.CountriesComposeAppTheme
 import com.androidbros.countries.view.CountryDetailScreen
 import com.androidbros.countries.view.CountryListScreen
 import com.androidbros.countries.view.SplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalAnimationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,6 +37,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalCoilApi
+@ExperimentalAnimationApi
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
---
+++ app/src/main/java/com/androidbros/countries/view/screens/CountryListScreen.kt
@@ -0,0 +1,48 @@
+package com.androidbros.countries.view
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import coil.annotation.ExperimentalCoilApi
+import com.androidbros.countries.view.components.CountryCard
+import com.androidbros.countries.view.components.TopBar
+import com.androidbros.countries.viewmodel.CountryListViewModel
+
+@ExperimentalCoilApi
+@ExperimentalAnimationApi
+@Composable
+fun CountryListScreen(navController: NavController,viewModel:CountryListViewModel= hiltViewModel()) {
+
+    val listState=viewModel.countriesList.observeAsState()
+
+
+    Scaffold(topBar = { TopBar()}) {
+
+
+        if (!listState.value.isNullOrEmpty()){
+
+            LazyColumn{
+                items(listState.value!!){ item ->
+
+                    CountryCard(navController,item)
+
+                }
+
+
+            }
+
+        }
+
+
+
+
+
+    }
+
+
+}
