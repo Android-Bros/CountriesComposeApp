1bd037c7106f153be5351dec76f6f46181a5da3a
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-09-11 15:38:48 +0300

    feature/add-navigation (#10)
    * navigation screens created
    
    * SplashScreen.kt completed

--- app/src/main/java/com/androidbros/countries/MainActivity.kt
+++ app/src/main/java/com/androidbros/countries/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.androidbros.countries.ui.theme.CountriesComposeAppTheme
+import com.androidbros.countries.view.CountryDetailScreen
+import com.androidbros.countries.view.CountryListScreen
+import com.androidbros.countries.view.SplashScreen
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -18,9 +23,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CountriesComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    Navigation()
                 }
             }
         }
@@ -28,14 +34,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CountriesComposeAppTheme {
-        Greeting("Android")
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ) {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+        composable("country_list_screen") {
+            CountryListScreen(navController = navController)
+        }
+        composable(
+            "country_detail_screen/{name}",
+            arguments = listOf(navArgument("name") {
+                type = NavType.StringType
+            })
+        ) { backStackEntry ->
+            CountryDetailScreen(
+                navController = navController,
+                name = backStackEntry.arguments?.getString("name")
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/androidbros/countries/view/CountryListScreen.kt
@@ -0,0 +1,9 @@
+package com.androidbros.countries.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CountryListScreen(navController: NavController) {
+
+}
