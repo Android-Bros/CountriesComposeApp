5a06290e662833305d799c694492fa103c37cd15
adematici <adematicis41@gmail.com>
2021-09-11 11:35:13 +0300

    api interface created

---
+++ app/src/main/java/com/androidbros/countries/network/CountriesAPI.kt
@@ -0,0 +1,15 @@
+package com.androidbros.countries.network
+
+import com.androidbros.countries.model.CountryItem
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CountriesAPI {
+
+    @GET("all")
+    suspend fun getAllCountries(): List<CountryItem>
+
+    @GET("name/{NAME}")
+    suspend fun getCountry(@Path("NAME") name: String): CountryItem
+
+}
