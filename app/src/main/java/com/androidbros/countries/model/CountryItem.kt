e69cb9d2c4de36694aec5ef5ece6f94eaeeaa1c5
adematici <adematicis41@gmail.com>
2021-09-11 11:16:01 +0300

    country models added

---
+++ app/src/main/java/com/androidbros/countries/model/CountryItem.kt
@@ -0,0 +1,34 @@
+package com.androidbros.countries.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CountryItem(
+    @SerializedName("alpha2Code")
+    val alpha2Code: String,
+    @SerializedName("altSpellings")
+    val altSpellings: List<String>,
+    @SerializedName("area")
+    val area: Double,
+    @SerializedName("capital")
+    val capital: String,
+    @SerializedName("currencies")
+    val currencies: List<Currency>,
+    @SerializedName("demonym")
+    val demonym: String,
+    @SerializedName("flag")
+    val flag: String,
+    @SerializedName("latlng")
+    val latlng: List<Double>,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("nativeName")
+    val nativeName: String,
+    @SerializedName("population")
+    val population: Int,
+    @SerializedName("region")
+    val region: String,
+    @SerializedName("subregion")
+    val subregion: String,
+    @SerializedName("timezones")
+    val timezones: List<String>
+)
---
+++ app/src/main/java/com/androidbros/countries/model/Currency.kt
@@ -0,0 +1,13 @@
+package com.androidbros.countries.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Currency(
+    @SerializedName("code")
+    val code: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("symbol")
+    val symbol: String
+)
